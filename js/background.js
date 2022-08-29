8079139b596a89818f3e99850a1109eaaf427c6d
kanemoto-r <10150406reika@gmil.com>
2022-08-29 12:52:22 +0900

    fixed

--- js/background.js
+++ js/background.js
@@ -16,7 +16,7 @@ particlesJS("particles-js", {
         "width": 0,
       },
       "image": {
-        "src": "/images/flower.png",//【重要】画像を指定！桜の画像を設定してください。
+        "src": "images/flower.png",//【重要】画像を指定！桜の画像を設定してください。
         "width": 120,
         "height": 120
       }
